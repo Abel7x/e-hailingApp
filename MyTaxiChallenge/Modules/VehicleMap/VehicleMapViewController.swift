a7916da7e81296ce79fe0287f1cbf2162571a323
Abel Martínez <ricardo.martinez@globant.com>
2018-07-17 15:27:53 -0500

    Added Logic to Map View Model and View Controller

--- MyTaxiChallenge/Modules/VehicleMap/VehicleMapViewController.swift
+++ MyTaxiChallenge/Modules/VehicleMap/VehicleMapViewController.swift
@@ -7,29 +7,136 @@
 //
 
 import UIKit
+import MapKit
+import NVActivityIndicatorView
 
 class VehicleMapViewController: UIViewController {
 
+    var activityIndicator: NVActivityIndicatorView = NVActivityIndicatorView(frame: CGRect.zero)
+    @IBOutlet weak var mapView: MKMapView!
+    
+    lazy var viewModel: VehicleMapViewModel = {
+        return VehicleMapViewModel()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupView()
+        configureActivityIndicator()
+        initViewModel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func setupView() {
+        self.navigationItem.title = "Vehicles Near Me"
     }
-    */
-
+    
+    private func configureActivityIndicator() {
+        let frame = CGRect(x: view.center.x, y: view.center.y, width: 100, height: 100)
+        activityIndicator = NVActivityIndicatorView(frame: frame, type: .ballScaleRippleMultiple, color: UIColor.white)
+        activityIndicator.center = CGPoint(x: view.center.x, y: view.center.y)
+        activityIndicator.isHidden = true
+        view.addSubview(activityIndicator)
+    }
+    
+    private func initViewModel() {
+        viewModel.showAlertClosure = { [weak self] () in
+            DispatchQueue.main.async { [weak self] in
+                guard let strongSelf = self else { return }
+                if let message = strongSelf.viewModel.alertMessage {
+                    strongSelf.showAlert( message )
+                }
+            }
+        }
+        
+        viewModel.updateLoadingStatus = { [weak self] () in
+            DispatchQueue.main.async { [weak self] in
+                guard let strongSelf = self else { return }
+                let isLoading = strongSelf.viewModel.isLoading
+                if isLoading {
+                    strongSelf.activityIndicator.isHidden = false
+                    strongSelf.activityIndicator.startAnimating()
+                }else {
+                    strongSelf.activityIndicator.isHidden = true
+                    strongSelf.activityIndicator.stopAnimating()
+                }
+            }
+        }
+        
+        viewModel.reloadAnnotations = { [weak self] () in
+            guard let strongSelf = self else { return }
+            DispatchQueue.main.async {
+                strongSelf.addVehiclesToMap(list: strongSelf.viewModel.getVehicleAnnotationViewModels())
+            }
+        }
+    }
+    
+    private func showAlert( _ message: String ) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction( UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
+
+extension VehicleMapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        
+        let annotationIdentifier = "AnnotationIdentifier"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView!.canShowCallout = true
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        
+        if let vehicleAnnotation = annotation as? VehicleAnnotation {
+            annotationView?.image = vehicleAnnotation.image
+        }
+        
+        
+        return annotationView
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        updateListOfCars()
+    }
+    
+    func updateListOfCars(){
+        let northEast = mapView.convert(CGPoint(x: mapView.bounds.width, y: 0), toCoordinateFrom: mapView)
+        let southWest = mapView.convert(CGPoint(x: 0, y: mapView.bounds.height), toCoordinateFrom: mapView)
+        
+        let nePoint = CLLocation(latitude: northEast.latitude, longitude: northEast.longitude)
+        let swPoint = CLLocation(latitude: southWest.latitude, longitude: southWest.longitude)
+        viewModel.getVehicles(at: nePoint, swPoint: swPoint)
+    }
+    
+    func addVehiclesToMap(list: [VehicleAnnotationViewModel]) {
+        mapView.removeAnnotations(mapView.annotations)
+        for vehicleAnnotation in list {
+            let radiansDirection = vehicleAnnotation.heading * .pi / 180
+            guard let image = UIImage(named: "car-top") else { return }
+            let rotatedImage = image.rotate(radians: CGFloat(radiansDirection))
+            let myAnnotation = VehicleAnnotation(with: CLLocationCoordinate2DMake(vehicleAnnotation.lat, vehicleAnnotation.lon),
+                                                                    image: rotatedImage)
+            myAnnotation.coordinate = CLLocationCoordinate2DMake(vehicleAnnotation.lat, vehicleAnnotation.lon);
+            mapView.addAnnotation(myAnnotation)
+        }
+    }
+}
+
+class VehicleAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var image: UIImage
+    var title: String?
+    
+    init(with coordinate: CLLocationCoordinate2D, image: UIImage, title: String = ""){
+        self.coordinate = coordinate
+        self.image = image
+    }
+}
+
--- MyTaxiChallenge/Modules/VehicleMap/VehicleMapViewModel.swift
+++ MyTaxiChallenge/Modules/VehicleMap/VehicleMapViewModel.swift
@@ -7,3 +7,91 @@
 //
 
 import Foundation
+
+class VehicleMapViewModel {
+    let serviceManager: APIServiceManager
+    
+    private var vehicles:[Vehicle] = [Vehicle]()
+    
+    var reloadAnnotations: (()->())?
+    var showAlertClosure: (()->())?
+    var updateLoadingStatus: (()->())?
+    
+    private var cellViewModels: [VehicleAnnotationViewModel] = [VehicleAnnotationViewModel]() {
+        didSet {
+            self.reloadAnnotations?()
+        }
+    }
+    
+    var isLoading: Bool = false {
+        didSet {
+            self.updateLoadingStatus?()
+        }
+    }
+    
+    var alertMessage: String? {
+        didSet {
+            self.showAlertClosure?()
+        }
+    }
+    
+    var numberOfCells: Int {
+        return cellViewModels.count
+    }
+    
+    init(serviceManager: APIServiceManager = APIServiceManager()) {
+        self.serviceManager = serviceManager
+    }
+    
+    func getVehicles(at nePoint: CLLocation, swPoint: CLLocation) {
+        self.isLoading = true
+        serviceManager.getVehiclesFromNePoint(nePoint, swPoint: swPoint, success:{ [weak self] (response) in
+            guard let strongSelf = self  else { return }
+            strongSelf.isLoading = false
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: response as Any, options: JSONSerialization.WritingOptions.prettyPrinted)
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                let vehicles = try! decoder.decode(Vehicles.self, from: jsonData)
+                strongSelf.processFetchedVehicles(vehicles: vehicles.poiList)
+            } catch {
+                strongSelf.alertMessage = "Unexpected Error"
+            }
+        }) { [weak self] (error, errorCode) in
+            guard let strongSelf = self else { return }
+            strongSelf.alertMessage = error
+        }
+    }
+
+    
+    func createAnnotationViewModel(vehicle: Vehicle) -> VehicleAnnotationViewModel {
+        return VehicleAnnotationViewModel(id: "\(vehicle.id)",
+            lat: vehicle.coordinate.latitude,
+            lon: vehicle.coordinate.longitude,
+            heading: vehicle.heading,
+            type: vehicle.type,
+            state: vehicle.state)
+    }
+    
+    private func processFetchedVehicles(vehicles: [Vehicle]) {
+        self.vehicles = vehicles
+        var viewModels = [VehicleAnnotationViewModel]()
+        for vehicle in vehicles {
+            viewModels.append(createAnnotationViewModel(vehicle: vehicle))
+        }
+        cellViewModels = viewModels
+    }
+    
+    func getVehicleAnnotationViewModels() -> [VehicleAnnotationViewModel] {
+        return cellViewModels
+    }
+}
+
+struct VehicleAnnotationViewModel {
+    let id: String
+    let lat: Double
+    let lon: Double
+    let heading: Double
+    let type: String
+    let state: String
+}
