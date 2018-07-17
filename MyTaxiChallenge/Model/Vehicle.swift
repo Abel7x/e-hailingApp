5664ec4ed7cc9bff221c5426549ea59bed2f099e
Abel Martínez <ricardo.martinez@globant.com>
2018-07-17 11:26:52 -0500

    Implemented Vehicle model

--- MyTaxiChallenge/Model/Vehicle.swift
+++ MyTaxiChallenge/Model/Vehicle.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+/*
+ {
+ "poiList": [{
+ "id": -479925044,
+ "coordinate": {
+ "latitude": 53.5530854,
+ "longitude": 9.955689
+ },
+ "state": "INACTIVE",
+ "type": "TAXI",
+ "heading": 0.0
+ }]
+ }
+ */
+
+struct Vehicles: Codable {
+    let poiList: [Vehicle]
+}
+
+struct Vehicle: Codable {
+    let id: Int
+    let coordinate: Coordinate
+    let state: String
+    let type: String
+    let heading: Double
+}
+
+struct Coordinate: Codable {
+    let latitude: Double
+    let longitude: Double
+}
